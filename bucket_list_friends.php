359f1acec69ce84ea23a7ffec48ddcc69db2abed
youp bos <2107418@talnet.nl>
2020-02-05 12:07:22 +0100

    Uitwerking van de exercise

--- bucket_list_friends.php
+++ bucket_list_friends.php
@@ -3,18 +3,22 @@ $arr = [];
 echo"Hoeveel vrienden zal ik vragen on hun droom?".PHP_EOL;
 
 $antwoord = readline("");
+if(is_numeric($antwoord)){
 
-for($i = 1; $i <= $antwoord; $i++){
-$a =  readline("wat is jouw naam?").PHP_EOL;
-$b =  readline("wat is jouw droom").PHP_EOL;
-
-$arr[$a] = [
-    'dream' => $b,
-];
+    for($i = 1; $i <= $antwoord; $i++){
+    $a =  readline("wat is jouw naam?").PHP_EOL;
+    $b =  readline("wat is jouw droom").PHP_EOL;
+    
+    $arr[$a] = [
+        'dream' => $b,
+    ];
+    }
+    
+    foreach($arr as $a => $data){
+        echo "{$a} heeft als droom : {$data['dream']}".PHP_EOL;
+    }
 }
-
-foreach($arr as $a => $data){
-    echo "{$a} heeft als droom : {$data['dream']}".PHP_EOL;
+else{
+    echo"dit is helaas geen getal!";
 }
-
 ?>
